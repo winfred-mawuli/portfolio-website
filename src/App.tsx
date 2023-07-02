0b9701c103d3df16341d6d8531bcba1c27671bd3
winfred-mawuli <winfredagbotey20@gmail.com>
2023-07-02 17:27:43 +0000

    feat: add a coming soon page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
+import {ComingSoonCounter} from "./components/ComingSoonCounter";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <ComingSoonCounter/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,21 @@
 import {navList} from "../constants/app_constanst";
+import {FcDownload} from "react-icons/fc"
 
 export const Header = () => {
     console.log(navList);
     return (
-        <div className="flex justify-evenly mt-5">
-            <h1 className="text-blue-900  font-extrabold">MawuliDev</h1>
+        <div className="flex justify-evenly mt-5 items-center">
+            <h1 className="text-blue-900  font-extrabold text-4xl">MawuliDev</h1>
             <nav className="ml-96">
-                <ul className="flex justify-evenly gap-10">
+                <ul className="flex justify-evenly gap-10 items-center">
                     {navList.map((item, index) => (
-                        <li key={index} className="hover:text-blue-900">{item}</li>
+                        <li key={index} className="hover:text-blue-900 hover:underline text-xl">{item}</li>
                     ))}
                 </ul>
             </nav>
-            <div className="active:bg-amber-500 h-auto">
-                <button>Download CV</button>
-            </div>
+            <button className="active:bg-amber-500 flex gap-3 items-center py-2 px-4 rounded cursor-pointer">Download CV<FcDownload/>
+            </button>
+
         </div>
     );
 };
